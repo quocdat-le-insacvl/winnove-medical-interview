d9ddbcd89fb32f64ea14f729dc3c855db54b47a7
LE Quoc Dat <quocdat-le-insacvl@gmail.com>
2022-03-31 16:18:14 -0700

    [feat]: read + write stl file using CGAL

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include <CGAL/boost/graph/IO/STL.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Surface_mesh.h>
+
+typedef CGAL::Simple_cartesian<double> K;
+typedef CGAL::Surface_mesh<K::Point_3> Mesh;
 
 int main(int argc, char* argv[]) {
     // Check the argument file name
     if (argc != 2) {
-        cout << "Required parameters: Filename(.stl)" << endl;
+        std::cout << "Required parameters: Filename(.stl)" << std::endl;
         return EXIT_FAILURE;
     }
 
     // Get the file name
     std::string inputFilename = argv[1];
+    std::cout << "Input file : " << inputFilename << std::endl;
 
     // create output file name, e.g : ../30_DEG/UPPER_30.stl => ../30_DEG/output_UPPER_30.stl
     std::string output_file;
@@ -17,12 +23,22 @@ int main(int argc, char* argv[]) {
         if (inputFilename[i] == '/') {
             break;
         }
-
     output_file = inputFilename.substr(0, i+1) + "output_" + inputFilename.substr(i+1, inputFilename.size() - i);
+    //-------------------------
 
-    bool CGAL::IO::read_STL 	( 	std::istream &  	is,
-                                    Graph &  	g,
-                                    const NamedParameters &  	np
-    )
+
+    // Read input .stl file
+    Mesh m;
+    std::ifstream is (inputFilename);
+    CGAL::IO::read_STL(is, m);
+    //-------------------------
+
+
+
+    // Write to file
+//    std::ofstream os(output_file);
+//    CGAL::IO::write_STL(os, m);
+//    std::cout << "Output file : " << output_file << std::endl;
+    //-------------------------
     return EXIT_SUCCESS;
 }
